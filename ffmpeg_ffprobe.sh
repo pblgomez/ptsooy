992e5dc97b390f5f70e242437513cc1b9d469d1c
Pablo Gómez <pablogomez@pablogomez.com>
2020-08-10 19:29:03 +0100

    Rename ffmpeg_ffprobe.sh and bumped to version 0.1.3

---
+++ ffmpeg_ffprobe.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+if uname -m | grep aarch64; then
+  curl -o /tmp/ffmpeg.tar.xz https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-arm64-static.tar.xz
+  tar xvf /tmp/ffmpeg.tar.xz
+  cp ffm*/ffmpeg /ffmpeg
+  cp ffm*/ffprobe /ffprobe
+elif uname -m | grep x86_64; then
+  curl -o /tmp/ffmpeg.tar.xz https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
+  tar xvf /tmp/ffmpeg.tar.xz
+  cp ffm*/ffmpeg /ffmpeg
+  cp ffm*/ffprobe /ffprobe
+elif uname -m | grep armv7l; then
+  curl -o /tmp/ffmpeg.tar.xz https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-armhf-static.tar.xz
+  tar xvf /tmp/ffmpeg.tar.xz
+  cp ffm*/ffmpeg /ffmpeg
+  cp ffm*/ffprobe /ffprobe
+fi
